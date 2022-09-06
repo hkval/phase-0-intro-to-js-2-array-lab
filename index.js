f96203bb19920cafaf06411f36a60aad2b91ea91
Holly <hollykvalheim@gmail.com>
2022-09-05 19:43:08 -0700

    commit tree 99551162845e679ccc2cbfb055bc7dd2deddba87

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+//test one
+function destructivelyAppendCat(name){
+    cats.push(name)
+};
+//test two
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+};
+//test three
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+//test four
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+};
+//test five
+function appendCat(name){
+    return [...cats, name]
+};
+//test six
+function prependCat(name){
+    return [name, ...cats]
+};
+//test seven
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+};
+//test eight
+function removeFirstCat(){
+    return cats.slice(1)
+};
